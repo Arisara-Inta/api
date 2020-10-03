167781fff67ff79e444214f4433b4332c2e26d3a
Arisara-Inta <aung_jaffy.140@hotmail.com>
2020-10-03 17:28:47 +0700

    Update

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <br />
+    <HelloWorld id="text" msg="MY MUSIC" />
+    <music />
+  </div>
+</template>
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import music from "./components/music.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    music,
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-size: cover;
+  background-image: url();
+}
+#text {
+  color: rgb(0, 0, 0);
+}
+</style>
